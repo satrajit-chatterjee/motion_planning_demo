e4260d5ae6def3639b121ac8c8b1b1d3ba6c99cb
Satrajit Chatterjee <satrajitdchatterjee@gmail.com>
2023-03-31 20:30:51 -0400

    Refactoring planner to allow for replanning

--- src/rrt_star_planner.cpp
+++ src/rrt_star_planner.cpp
@@ -11,51 +11,84 @@
 #include <ompl/base/objectives/PathLengthOptimizationObjective.h>
 #include <ompl/geometric/planners/rrt/RRTstar.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <nav_msgs/Odometry.h>
+#include <ompl_rrtstar/ReplanTrigger.h>
 
 namespace ob = ompl::base;
 namespace og = ompl::geometric;
 
 ros::Publisher plan_pub;
+std::string frame_id = "odom";
+
+ros::Subscriber pointcloud_sub_;
+sensor_msgs::PointCloud2::ConstPtr global_msg = nullptr;
+bool has_map_ = false;
+
+bool first_plan = true;
+ompl::base::SE3StateSpace::StateType* global_state = nullptr;
+bool has_odom_ = false;
+
+bool start_replan_service = false;
 
 class RRTStarPlanner {
 public:
     ros::NodeHandle nh_;
-    bool has_map_;
-    double _resolution;
-    double _map_wait;
-    double plan_duration_;
-    double obs_constraint_;
-    std::string map_frame_id;
-    sensor_msgs::PointCloud2::ConstPtr global_msg = nullptr;
-    
+    double _resolution = 0.1;
+    double _map_wait = 1.0;
+    double plan_duration_ = 1.0;
+    double obs_constraint_ = 0.05;
+    std::string map_frame_id;    
 
     RRTStarPlanner(ros::NodeHandle& nh) 
-    : nh_(nh){
-        this->has_map_ = false;
-    }
+    : nh_(nh){}
 
     void pointcloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg) {
         // Convert the ROS message to a PCL point cloud
 
         global_msg = msg;
 
-        this->has_map_ = true;
+        has_map_ = true;
+    }
+
+    void odomCallback(const nav_msgs::Odometry::ConstPtr &msg) {
+        // Extract position and orientation from the message
+        double x = msg->pose.pose.position.x;
+        double y = msg->pose.pose.position.y;
+        double z = msg->pose.pose.position.z;
+    
+        double qx = msg->pose.pose.orientation.x;
+        double qy = msg->pose.pose.orientation.y;
+        double qz = msg->pose.pose.orientation.z;
+        double qw = msg->pose.pose.orientation.w;
+
+        // Create a new state with the extracted position and orientation
+        ompl::base::SE3StateSpace::StateType* state = new ompl::base::SE3StateSpace::StateType();
+    
+        state->setXYZ(x, y, z);
+    
+        state->rotation().x = qx;
+        state->rotation().y = qy;
+        state->rotation().z = qz;
+        state->rotation().w = qw;
+
+        // Update global state and set the new data flag
+        global_state = state;
+        has_odom_ = true;
+        ROS_INFO("Found new start state!");
     }
 
 
     void startPlanning() {
         // Reading roslaunch params
-        nh_.param("map/resolution", _resolution, 0.1);
         nh_.param("planner/map_wait_duration", _map_wait, 1.0);
         nh_.param("planner/plan_duration", plan_duration_, 1.0);
         nh_.param("planner/obs_constraint", obs_constraint_, 1.0);
 
         // Subscribe to the point cloud topic
-        ros::Subscriber pointcloud_sub_;
         pointcloud_sub_ = nh_.subscribe("/structure_map/global_cloud", 1, &RRTStarPlanner::pointcloudCallback, this);
 
         // Wait for the first point cloud message to arrive
-        while (!this->has_map_) {
+        while (!has_map_) {
             ROS_INFO("Waiting for map generation...");
             ros::spinOnce();
             ros::Duration(_map_wait).sleep();
@@ -87,31 +120,55 @@ public:
         // Create a simple setup with the state space
         og::SimpleSetup ss(space);
 
-        // Set a random start state
         ob::ScopedState<ob::SE3StateSpace> start(space);
-        bool start_valid = false;
-        ROS_INFO("Setting random start position...");
-        while (!start_valid) {
-            start.random();
-            float x = start->getX();
-            float y = start->getY();
-            float z = start->getZ();
+        
+        if(first_plan) {
+            // Set a random start state
+            bool start_valid = false;
+            ROS_INFO("Setting random start position...");
+            while (!start_valid) {
+                start.random();
+                float x = start->getX();
+                float y = start->getY();
+                float z = start->getZ();
 
-            // Check if the point is in collision with any obstacle in the point cloud
-            pcl::PointXYZ point(x, y, z);
-            std::vector<int> indices;
-            std::vector<float> sqr_dists;
-            octree_.nearestKSearch(point, 1, indices, sqr_dists);
-            if (sqr_dists[0] > obs_constraint_) {
-                start_valid = true;
+                // Check if the point is in collision with any obstacle in the point cloud
+                pcl::PointXYZ point(x, y, z);
+                std::vector<int> indices;
+                std::vector<float> sqr_dists;
+                octree_.nearestKSearch(point, 1, indices, sqr_dists);
+                if (sqr_dists[0] > obs_constraint_) {
+                    start_valid = true;
+                }
             }
+
+            ROS_INFO("The random start state is::");
+            ROS_INFO("X::%f::Y::%f::Z::%f", start->getX(), start->getY(), start->getZ());
+
+            // Set both start and goal states to random orientations
+            double start_q1 = ompl::RNG().uniformReal(0, 2 * M_PI);
+            double start_q2 = ompl::RNG().uniformReal(0, 2 * M_PI);
+            double start_q3 = ompl::RNG().uniformReal(0, 2 * M_PI);
+            double start_q4 = ompl::RNG().uniformReal(0, 2 * M_PI);
+            start->as<ob::SO3StateSpace::StateType>(1)->setAxisAngle(start_q1, start_q2, start_q3, start_q4);
+            
+            first_plan = false;
         }
-
-        ROS_INFO("The random start state is::");
-        ROS_INFO("X::%f::Y::%f::Z::%f", start->getX(), start->getY(), start->getZ());
-        // bool start_validity = isStateValid(start, octree_);
-
-        // Set a andom goal state
+        else {
+            // Subscribe to current odom and set as start position
+            ros::Subscriber plan_sub_;
+            plan_sub_ = nh_.subscribe("/quadrotor/odom", 1, &RRTStarPlanner::odomCallback, this);
+            
+            while (!has_odom_) {
+                ROS_INFO("Waiting to obtain present odometry...");
+                ros::spinOnce();
+                ros::Duration(_map_wait).sleep();
+            }
+            start = global_state;
+            has_odom_ = false;
+        }
+        
+        // Set a random goal state
         ob::ScopedState<ob::SE3StateSpace> goal(space);
         bool goal_valid = false;
         ROS_INFO("Setting random goal position...");
@@ -134,19 +191,13 @@ public:
         ROS_INFO("The random goal state is::");
         ROS_INFO("X::%f::Y::%f::Z::%f", goal->getX(), goal->getY(), goal->getZ());
 
-        // Set both start and goal states to random orientations
-        double start_q1 = ompl::RNG().uniformReal(0, 2 * M_PI);
-        double start_q2 = ompl::RNG().uniformReal(0, 2 * M_PI);
-        double start_q3 = ompl::RNG().uniformReal(0, 2 * M_PI);
-        double start_q4 = ompl::RNG().uniformReal(0, 2 * M_PI);
-        start->as<ob::SO3StateSpace::StateType>(1)->setAxisAngle(start_q1, start_q2, start_q3, start_q4);
-
         double goal_q1 = ompl::RNG().uniformReal(0, 2 * M_PI);
         double goal_q2 = ompl::RNG().uniformReal(0, 2 * M_PI);
         double goal_q3 = ompl::RNG().uniformReal(0, 2 * M_PI);
         double goal_q4 = ompl::RNG().uniformReal(0, 2 * M_PI);
         goal->as<ob::SO3StateSpace::StateType>(1)->setAxisAngle(goal_q1, goal_q2, goal_q3, goal_q4);
 
+        
         ss.setStartAndGoalStates(start, goal);
 
         // Set the planner
@@ -179,7 +230,7 @@ public:
         ss.setOptimizationObjective(obj);
 
 
-        // Attempt to solve the problem within 1 second of planning time
+        // Attempt to solve the problem within given planning time
         ob::PlannerStatus solved = ss.solve(plan_duration_);
  
         if (solved) {
@@ -194,8 +245,8 @@ public:
             ros::Rate loop_rate(10);
             while (ros::ok()) {
                 publishPlan(path);
-
-                // ROS_INFO("Path published!");
+        
+                // TODO: Have a service call to reinitiate planning
 
                 // Call any pending callbacks
                 ros::spinOnce();
@@ -216,7 +267,7 @@ public:
         nav_msgs::Path plan_msg;
 
         // Set the header of the path message
-        plan_msg.header.frame_id = "odom";
+        plan_msg.header.frame_id = frame_id;
         plan_msg.header.stamp = ros::Time::now();
 
         // Add each state to the path message
@@ -226,7 +277,7 @@ public:
             geometry_msgs::PoseStamped pose_stamped;
 
             // Set the header of the pose stamped message
-            pose_stamped.header.frame_id = map_frame_id;
+            pose_stamped.header.frame_id = frame_id;
             pose_stamped.header.stamp = ros::Time::now();
 
             // Set the position of the pose stamped message to the state's X, Y, and Z values
@@ -256,12 +307,28 @@ public:
 
 };
 
+
+bool replanCallback(ompl_rrtstar::ReplanTrigger::Request &req, 
+                    ompl_rrtstar::ReplanTrigger::Response &res) {
+    
+     ros::NodeHandle nh("~");
+
+    RRTStarPlanner planner_obj(nh);
+    planner_obj.startPlanning();
+    res.success = true;
+    return true;
+}
+
+
 int main (int argc, char** argv){
     ros::init(argc, argv, "rrt_plan_node");
     ros::NodeHandle nh("~");
     // Create a ROS publisher for the plan
     plan_pub = nh.advertise<nav_msgs::Path>("planner/rrt_plan", 1);
+    ros::ServiceServer service = nh.advertiseService("replan", replanCallback);
+
     RRTStarPlanner planner_obj(nh);
     planner_obj.startPlanning();
+
     return 0;
 }
